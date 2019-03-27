880b56f65f209723867e91dcc074ca31327e69da
BackrndSource <back.rndsource@gmail.com>
2019-03-27 15:47:19 +0000

    Create es.lua

---
+++ locales/es.lua
@@ -0,0 +1,12 @@
+Locales ['es'] = {
+  ['blip_cinema'] = 'sala de cine',
+  ['menu_item'] = '%s€',
+
+  -- cinema
+  ['cinema_prompt'] = 'pulsa ~INPUT_CONTEXT~ para entrar al ~y~cine~s~.',
+  ['cinema_prompt_closed'] = 'las salas de cine están ~r~cerradas~s~, vuelve entre las 7 y las 11 de la tarde.',
+  ['cinema_menutitle'] = 'películas disponibles',
+  ['cinema_entered'] = 'pulsa ~r~E~s~ para salir de la sala de cine',
+  ['cinema_poor'] = 'no tienes suficiente dinero...',
+  ['cinema_youpaid'] = 'has pagado la entrada al cine ~r~%s€~s~',
+}
